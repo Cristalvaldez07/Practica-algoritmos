58e0ff5b4272dbc0f27530ed43066d25d70186a6
Cristalvaldez07 <vzcristal@gmail.coml>
2021-11-28 14:48:53 -0400

    exercise10

---
+++ Exercises/cuadratica.js
@@ -0,0 +1,8 @@
+/*Haga una función eqcuad(a, b, c) la cual retorne la solución positiva de una ecuación cuadrática 
+cuyos parámetros son a, b y c respectivamente. */
+
+var eqcuad = function(a, b, c) {
+    let x = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    let y = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    return x;
+}

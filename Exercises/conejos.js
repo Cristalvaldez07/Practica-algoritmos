46699e5072656ebea5eff14e6ca94eb5b1daffd4
Cristalvaldez07 <vzcristal@gmail.coml>
2021-11-28 14:45:18 -0400

    FIBONACCI EXERCISE7

---
+++ Exercises/conejos.js
@@ -0,0 +1,18 @@
+/* Los conejos de Fibonacci. El matemático Leonardo Fibonacci expuso el siguiente problema,
+haga un programa para solucionarlo: Suponga que una pareja de conejos tiene un par de
+crías cada mes y a su vez las crías se hacen fértiles al cabo de un mes y empiezan a tener sus
+propias crías. Si comenzamos con una pareja fértil y no muere, ¿Cuántos pares de conejos se
+tendrán al cabo de un año?. */
+
+const fibonacci = (n) => {
+
+    // Base case
+    if (n < 2) {
+        return n;
+    }
+
+    // Recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+fibonacci(12);

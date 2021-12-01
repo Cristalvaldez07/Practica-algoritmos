71dc1d63531b528ef8152ff9a229669d5a80703e
Cristalvaldez07 <vzcristal@gmail.coml>
2021-12-01 12:10:54 -0400

    Banco finally

--- Exercises/banco.js
+++ Exercises/banco.js
@@ -1,7 +1,20 @@
 /* Haga un programa al cual se le digite el monto a ahorrar, la tasa de
 interés, los meses de ahorro y que imprima por consola el monto al final del ahorro. */
 
-var monto = prompt("Ingrese el monto a ahorrar");
-var tasa = prompt("Ingrese la tasa de interés");
-var meses = prompt("Ingrese los meses de ahorro");
-console.log("El monto al final del ahorro es: " + (monto * Math.pow(1 + tasa / 100, meses)));
+
+  const interes = 4;
+  const capital = 500;
+  const meses = 2;
+  let result = capital;
+
+  function calcularInteres(interes, capital, meses){
+    for (let i = 0; i < meses; ++i){
+
+result += result * ( interes / 100) ;
+}
+ return result;
+
+  }
+
+
+console.log(calcularInteres(interes, capital, meses));

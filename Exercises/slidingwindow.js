8f1958acc21a475ec78d0ed6d3f1cc014e709f19
Cristalvaldez07 <vzcristal@gmail.coml>
2021-12-06 09:28:17 -0400

    Sliding Window

--- Exercises/slidingwindow.js
+++ Exercises/slidingwindow.js
@@ -1,4 +1,10 @@
 
+/* Dada una matriz de números positivos y un número positivo "k", encuentre la suma máxima 
+de cualquier submatriz contigua de tamaño "k".
+Input: [2, 1, 5, 1, 3, 2],  k=3 
+Output: 9
+Resultado con la máxima suma es [5, 1, 3] = 5+1+3 = 9. */
+
 
 const slidingWindow = (arr, k) => {
     let start= 0;
